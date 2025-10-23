a7a6804b08c2d1ab32b749857d1ae0c1eeaed08d
Pongsatorn-Notch <s6610886122@pkru.ac.th>
2025-10-23 12:41:46 +0700

    Update MultiList_app.py

--- MultiList_app.py
+++ MultiList_app.py
@@ -15,6 +15,7 @@ pages = [
 # สร้างเมนูนำทาง
 st.navigation(pages)
 
-st.title("Welcome to the Home Page")
-st.write("Click on a page in the sidebar to navigate.")
+# st.title("Welcome to the Home Page")
+# st.write("Click on a page in the sidebar to navigate.")
+
 
