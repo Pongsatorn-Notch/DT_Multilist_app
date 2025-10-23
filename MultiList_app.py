8b85a8d8ae9632322314cd6751d1cd158b4a5703
Pongsatorn-Notch <s6610886122@pkru.ac.th>
2025-10-23 12:40:31 +0700

    Update MultiList_app.py

--- MultiList_app.py
+++ MultiList_app.py
@@ -8,8 +8,8 @@ st.set_page_config(
 # กำหนดหน้าต่างๆ
 pages = [
     st.Page("MultiList_app.py", title="Home", icon="🏠"),
-    st.Page("pages/Profile.py", title="Profile", icon="👤"),
-    st.Page("pages/Hobby.py", title="Hobby", icon="🎮"),
+    st.Page("pages/profile.py", title="Profile", icon="👤"),
+    st.Page("pages/hobby.py", title="Hobby", icon="🎮"),
 ]
 
 # สร้างเมนูนำทาง
@@ -17,3 +17,4 @@ st.navigation(pages)
 
 st.title("Welcome to the Home Page")
 st.write("Click on a page in the sidebar to navigate.")
+
