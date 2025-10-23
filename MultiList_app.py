7afc0b61e590bb534b25b70ca36b2118ea1a1a23
Pongsatorn-Notch <s6610886122@pkru.ac.th>
2025-10-23 12:14:19 +0700

    Add files via upload

---
+++ MultiList_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Multipage App",
+    page_icon="👋",
+)
+
+# กำหนดหน้าต่างๆ
+pages = [
+    st.Page("MultiList_app.py", title="Home", icon="🏠"),
+    st.Page("Pages/1_Profile.py", title="Profile", icon="👤"),
+    st.Page("Pages/2_Hobby.py", title="Hobby", icon="🎮"),
+]
+
+# สร้างเมนูนำทาง
+st.navigation(pages)
+
+st.title("Welcome to the Home Page")
+st.write("Click on a page in the sidebar to navigate.")
